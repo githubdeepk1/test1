58d69afd98b0db333c046f447c274d434bbbc58c
deepak <deepak.kachawah@gmail.com>
2018-10-07 09:34:53 -0700

    mvn

---
+++ pro1/src/main/java/pck1/pro1/llll.java
@@ -0,0 +1,14 @@
+package pck1.pro1;
+
+public class llll {
+
+	public llll() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
